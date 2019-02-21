37dc60dc00541de29bbf6e64773cd2ed837d1c4c
Ellie Harrison <ellie@vertigomoon.co.uk>
2019-02-21 23:38:35 +0000

    Add redirect to source repo at root address

---
+++ src/test/java/uk/co/vertigomoon/htmltopdf/controller/HomeControllerTest.java
@@ -0,0 +1,37 @@
+package uk.co.vertigomoon.htmltopdf.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc webClient;
+
+    @Value("${application.github-redirect}")
+    private String redirectUrl;
+
+
+    @Test
+    public void rootShouldRedirectToRepositoryHome() throws Exception {
+        webClient.perform(get("/"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl(redirectUrl));
+    }
+
+}
+
+
